d508da321d0b49881af50272144de1b54e56f8ef
Damilare <damilare@Damilares-MacBook-Air.local>
2022-02-25 20:19:59 +0100

    Added BASE DIR

--- projectapp/settings.py
+++ projectapp/settings.py
@@ -138,12 +138,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
-STATIC_ROOT = 'BASE_DIR / static'
+#STATIC_ROOT = 'BASE_DIR / static'
 STATIC_URL = '/static/'
 STATICFILES_DIR = 'BASE_DIR /static'
 
 #STATIC_ROOT = 'BASE_DIR / static/'
-#STATIC_ROOT = 'BASE_DIR / static_files'
+STATIC_ROOT = 'BASE_DIR / static_files'
 #STATIC_URL = '/static/'
 #STATICFILES_DIR = 'BASE_DIR /static/'
 #STATICFILES_DIR = os.path.join(BASE_DIR, 'static/')
