58dbb1dc31b3427ae423be26d33e832538fe8608
Damilare <damilare@Damilares-MacBook-Air.local>
2022-02-25 19:46:50 +0100

    Modified staticfiles

--- projectapp/settings.py
+++ projectapp/settings.py
@@ -137,7 +137,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = 'BASE_DIR / static/'
+#STATIC_ROOT = 'BASE_DIR / static/'
+STATIC_ROOT = 'BASE_DIR / static_files'
 STATIC_URL = '/static/'
 
 STATICFILES_DIR = 'BASE_DIR /static/'
