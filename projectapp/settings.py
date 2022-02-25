a6ccf7067746f2056a4192c7dd25f18ec6e0e94e
Samuel Damilare <48826043+DPsalmist@users.noreply.github.com>
2022-02-26 00:50:50 +0100

    Update settings.py

--- projectapp/settings.py
+++ projectapp/settings.py
@@ -144,7 +144,7 @@ STATIC_URL = '/static/'
 
 #STATIC_ROOT = 'BASE_DIR / static'
 STATIC_URL = '/static/'
-STATICFILES_DIR = 'BASE_DIR /static'
+STATICFILES_DIRS = 'BASE_DIR /static'
 
 #STATIC_ROOT = 'BASE_DIR / static/'
 #STATIC_ROOT = 'BASE_DIR / staticfiles'
